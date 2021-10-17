7f2927ecd4cfbd93abdb2e4ffda90ec2a762fca0
Cristi Cretu <crisemcr@gmail.com>
2021-10-17 16:15:53 +0300

    Adds use-codemirror config

---
+++ packages/renderer/src/use-codemirror.tsx
@@ -0,0 +1,71 @@
+import { useEffect, useState, useRef } from 'react'
+import { EditorState } from '@codemirror/state'
+import { EditorView, keymap, highlightActiveLine } from '@codemirror/view'
+import { defaultKeymap } from '@codemirror/commands'
+import { history, historyKeymap } from '@codemirror/history'
+import { indentOnInput } from '@codemirror/language'
+import { bracketMatching } from '@codemirror/matchbrackets'
+import { lineNumbers, highlightActiveLineGutter } from '@codemirror/gutter'
+import { defaultHighlightStyle } from '@codemirror/highlight'
+import { markdown, markdownLanguage } from '@codemirror/lang-markdown'
+import { languages } from '@codemirror/language-data'
+import { oneDark } from '@codemirror/theme-one-dark'
+import type React from 'react'
+
+interface Props {
+  initialDoc: string,
+  onChange?: (state: EditorState) => void
+}
+
+const useCodeMirror = <T extends Element>(
+  props: Props
+): [React.MutableRefObject<T | null>, EditorView?] => {
+  const refContainer = useRef<T>(null)
+  const [editorView, setEditorView] = useState<EditorView>()
+  const { onChange } = props
+
+  useEffect(() => {
+    if (!refContainer.current) return
+
+    const startState = EditorState.create({
+      doc: props.initialDoc,
+      extensions: [
+        keymap.of([...defaultKeymap, ...historyKeymap]),
+        lineNumbers(),
+        highlightActiveLineGutter(),
+        history(),
+        indentOnInput(),
+        bracketMatching(),
+        defaultHighlightStyle.fallback,
+        highlightActiveLine(),
+        markdown({
+          base: markdownLanguage,
+          codeLanguages: languages,
+          addKeymap: true
+        }),
+        oneDark,
+        // transparentTheme,
+        // syntaxHighlighting,
+        EditorView.lineWrapping,
+        EditorView.updateListener.of(update => {
+          if (update.changes) {
+            onChange && onChange(update.state)
+          }
+        })
+      ]
+    })
+
+    const view = new EditorView({
+      state: startState,
+      parent: refContainer.current
+    })
+
+    setEditorView(view)
+
+  }, [refContainer])
+
+  return [refContainer, editorView]
+}
+
+
+export default useCodeMirror
